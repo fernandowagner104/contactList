b2e9cb3e2fc69c442896299083e3a6779f7bbcbb
fernandowagner104 <fernandowagner104@gmail.com>
2021-08-27 20:11:46 -0300

    Foi terminado a implementacao o recyclerView e o xml do contato

--- app/src/main/java/com/fernandowagner104/contactlist/ContactAdapter.kt
+++ app/src/main/java/com/fernandowagner104/contactlist/ContactAdapter.kt
@@ -1,29 +1,45 @@
 package com.fernandowagner104.contactlist
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactAdapterViewHolder>() {
 
     private val list: MutableList<Contact> = mutableListOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapterViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item,parent,false)
+        return ContactAdapterViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ContactAdapterViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(list[position])
     }
 
     override fun getItemCount(): Int { //
         return list.size
     }
 
+    fun updateList(list: List<Contact>) {
+        this.list.clear()
+        this.list.addAll(list)
+        notifyDataSetChanged()
+    }
+
     class ContactAdapterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val txtName: TextView = itemView.findViewById(R.id.txtName)
+        private val txtPhone: TextView = itemView.findViewById(R.id.txtPhone)
+        private val imgPhotograph: ImageView = itemView.findViewById(R.id.imgPhotograph)
 
         fun bind(contact: Contact) {
+            txtName.text = contact.name
+            txtPhone.text = contact.phone
 
         }
 
--- app/src/main/java/com/fernandowagner104/contactlist/MainActivity.kt
+++ app/src/main/java/com/fernandowagner104/contactlist/MainActivity.kt
@@ -2,10 +2,40 @@ package com.fernandowagner104.contactlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private val recViewList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.recView_list)
+    }
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
+        updateList()
+    }
+
+    private fun bindViews() {
+        recViewList.adapter = adapter
+        recViewList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList() {
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    name = "Fernando Wagner",
+                    phone = "(00)00000-0000",
+                    photograph = "img.png"),
+                Contact(
+                    name = "Ana",
+                    phone = "00",
+                    photograph = "img.png")
+            )
+        )
     }
 }
