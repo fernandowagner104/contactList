51f34aa574f302fc2f00459d5d311013fdbbff24
fernandowagner104 <fernandowagner104@gmail.com>
2021-08-29 12:10:35 -0300

    Mudança na aparência, implementação do drawer e toolbar

--- app/src/main/java/com/fernandowagner104/contactlist/MainActivity.kt
+++ app/src/main/java/com/fernandowagner104/contactlist/MainActivity.kt
@@ -2,6 +2,15 @@ package com.fernandowagner104.contactlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+//import android.widget.Toolbar
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -13,11 +22,23 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
 
+        initDrawer()
         bindViews()
         updateList()
     }
+
+    private fun initDrawer() {
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_drawer,R.string.close_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
 
     private fun bindViews() {
         recViewList.adapter = adapter
@@ -32,10 +53,35 @@ class MainActivity : AppCompatActivity() {
                     phone = "(00)00000-0000",
                     photograph = "img.png"),
                 Contact(
-                    name = "Ana",
-                    phone = "00",
+                    name = "Thierry Henry",
+                    phone = "(00)00000-0000",
                     photograph = "img.png")
             )
         )
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_menu_1 -> {
+                showToast("Exibindo Menu 1")
+                true
+
+            }
+            R.id.item_menu_2 -> {
+                showToast("Exibindo Menu 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
